7641498cfeec5bf4bdc5a51a7334cf49bcd56d48
Rddavis1389 <rddavis1389@yahoo.com>
2021-05-27 20:42:06 -0500

    multiple rounds

--- assets/js/game.js
+++ assets/js/game.js
@@ -79,7 +79,15 @@ var fight = function(enemyName) {
 
 // run fight function to start game
 for(var i = 0; i < enemyNames.length; i++) {
+    if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators!" + ( i + 1 ) );
     var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50
+    enemyHealth = 50;
+
     fight(pickedEnemyName);
+    }
+    else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+    }
 }
